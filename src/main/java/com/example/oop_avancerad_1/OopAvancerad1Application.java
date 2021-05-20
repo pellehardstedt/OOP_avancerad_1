8bb18d4e8f6df981121572fa7da6108de80ecc40
pellehardstedt <pelle.hardstedt>
2021-05-20 10:57:31 +0200

    first

---
+++ src/main/java/com/example/oop_avancerad_1/OopAvancerad1Application.java
@@ -0,0 +1,13 @@
+package com.example.oop_avancerad_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopAvancerad1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopAvancerad1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oop_avancerad_1/OopAvancerad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oop_avancerad_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopAvancerad1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
